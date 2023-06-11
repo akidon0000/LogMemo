4bce73db9226cd23af021daba4c49ec61009e348
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-06-11 09:05:11 +0900

    start

---
+++ LogMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogMemo
+//
+//  Created by Hina on 2023/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
